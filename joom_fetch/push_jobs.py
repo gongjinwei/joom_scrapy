6d205c9d7d08b91a9d08c597c811c73b0389f2d3
gongjinwei <jwakx@163.com>
2018-10-17 11:23:58 +0800

    push_job

--- joom_fetch/push_jobs.py
+++ joom_fetch/push_jobs.py
@@ -3,7 +3,7 @@ import os,sys
 import pickle,redis
 import django
 
-sys.path.append('D:\code\joom_scrapy')
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ['DJANGO_SETTINGS_MODULE']='joom_scrapy.settings'
 django.setup()
 from fetch.models import ItemUrl
@@ -12,3 +12,4 @@ client = redis.StrictRedis('122.226.65.250',18003)
 
 for item in ItemUrl.objects.filter(state=0):
     client.lpush('joom:start_urls',pickle.dumps(item.url_str))
+
