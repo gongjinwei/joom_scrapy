04fdfae232170baaa0f23e676100fe01b8900edf
gongjinwei <jwakx@163.com>
2018-10-23 22:29:22 +0800

    wish直接入库

--- joom_fetch/push_jobs.py
+++ joom_fetch/push_jobs.py
@@ -19,10 +19,10 @@ client = redis.StrictRedis('122.226.65.250',18003)
 # for url in WishShop.objects.filter(state=1).values('url'):
 #     client.lpush('wish:start_urls',url['url'])
 #
-# Shop.objects.filter(platform_id=9,status__lt=3).update(status=1)
-# for token in Shop.objects.filter(platform_id=9,status=1).values('access_token','id'):
-#     client.lpush('wish_api:start_urls', '%s+%s' %(token['id'],token['access_token']))
+Shop.objects.filter(platform_id=9,status__lt=3).update(status=1)
+for token in Shop.objects.filter(platform_id=9,status=1).values('access_token','id'):
+    client.lpush('wish_api:start_urls', '%s+%s' %(token['id'],token['access_token']))
 
-
-for wish_product in WishCrawlProduct.objects.all():
-    client.lpush('wish_rating:start_urls',wish_product.source_id)
+#
+# for wish_product in WishCrawlProduct.objects.all():
+#     client.lpush('wish_rating:start_urls',wish_product.source_id)
